c72b7738f04be57a74b67d95378c1e14e4e6c1b4
KhuatCong <khuatphucong06092002@gmail.com>
2023-09-25 17:32:27 +0700

    update

---
+++ js/main.js
@@ -0,0 +1,75 @@
+$(function () {
+    $('.slide-banner').slick({
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        infinite: true,
+    });
+
+    // $('.list-new-product').slick({
+    //     slidesToShow:4,
+    //     slidesToScroll:1,
+    //     infinite:true,
+    //     arrows:false,
+    //     autoplay:true,
+    //     autoplaySpeed:1000,
+    // });
+
+    $('.list-new-product').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        speed: 500,
+        prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+        nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    autoplay: true,
+                    autoplaySpeed: 1000,
+                    prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+                    nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    autoplay: true,
+                    autoplaySpeed: 1000,
+                    prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+                    nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 1000,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 1000,
+                }
+            }
+
+        ]
+    });
+})
