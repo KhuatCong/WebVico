ed865bd3475554e01b3938b89642141bd7aa101d
KhuatCong <khuatphucong06092002@gmail.com>
2023-09-26 12:01:43 +0700

    update

--- js/main.js
+++ js/main.js
@@ -19,6 +19,64 @@ $(function () {
         slidesToShow: 5,
         slidesToScroll: 1,
         arrows: true,
+        // autoplay: true,
+        autoplaySpeed: 1000,
+        speed: 500,
+        prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+        nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    autoplay: true,
+                    autoplaySpeed: 1000,
+                    prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+                    nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    autoplay: true,
+                    autoplaySpeed: 1000,
+                    prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+                    nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 1000,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    autoplay: true,
+                    autoplaySpeed: 1000,
+                }
+            }
+
+        ]
+    });
+
+    $('.list-danhmuc-sanpham').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        arrows: true,
         autoplay: true,
         autoplaySpeed: 1000,
         speed: 500,
@@ -71,5 +129,14 @@ $(function () {
             }
 
         ]
+    });
+
+    $('.slide-khachhang').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        prevArrow: '<div class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>',
+        nextArrow: '<div class="slick-next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>',
+        centerMode:true,
     });
 })
